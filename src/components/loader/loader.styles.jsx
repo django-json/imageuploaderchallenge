9801269c226ecd510a8f18c9b31a38666963c7d6
django-json <diango.jayson@gmail.com>
2022-01-21 10:58:12 +0800

    fix bug

--- src/components/loader/loader.styles.jsx
+++ src/components/loader/loader.styles.jsx
@@ -6,26 +6,56 @@ export const LoaderContainer = styled.div`
     height: 6px;
     background: var(--white-3);
     border-radius: 8px;
+    overflow-x: hidden;
 `;
 
 export const LoaderContent = styled.div`
     position: absolute;
     top: 0;
-    left: 0;
+    left: -100px;
     width: 100px;
     height: 6px;
     background: var(--blue-1);
     border-radius: 8px;
     animation: right 1s linear infinite;
-    -webkit-animation: right 2s linear infinite;
+    -webkit-animation: right 1s linear infinite;
     @keyframes right {
         to {
-            -webkit-transform: translateX(240px);
+            -webkit-transform: translateX(440px);
         }
     }
     @-webkit-keyframes right {
         to {
-            -webkit-transform: translate(240px);
+            -webkit-transform: translate(440px);
+        }
+    }
+
+    @media (max-width: 375px) {
+        @keyframes right {
+            to {
+                -webkit-transform: translateX(395px);
+            }
+        }
+        @-webkit-keyframes right {
+            to {
+                -webkit-transform: translate(395px);
+            }
+        }
+    }
+
+    @media (max-width: 374px) {
+        width: 70px;
+        left: -70px;
+
+        @keyframes right {
+            to {
+                -webkit-transform: translateX(310px);
+            }
+        }
+        @-webkit-keyframes right {
+            to {
+                -webkit-transform: translate(310px);
+            }
         }
     }
 `;
